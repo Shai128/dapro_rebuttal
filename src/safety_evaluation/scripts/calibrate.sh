a3844a18b7ea610aa10359b4326372f2b24c017d
shai128 <shai120899@gmail.com>
2026-07-26 19:14:44 +0300

    fix

--- src/safety_evaluation/scripts/calibrate.sh
+++ src/safety_evaluation/scripts/calibrate.sh
@@ -8,16 +8,16 @@ git pull
 
 setups=(
   'attack_toxic_attack_qwen25_14b_instruct_lm_target_qwen25_14b_instruct_judge_detoxify'
-# 'attack_toxic_attack_qwen25_14b_instruct_lm_target_mini_phi_4_instruct_judge_detoxify'\
-# 'attack_toxic_attack_qwen25_14b_instruct_lm_target_llama_31_8B_instruct_judge_detoxify'
-# 'attack_toxic_attack_qwen25_14b_instruct_lm_target_gemma3_4b_it_judge_detoxify'
+ 'attack_toxic_attack_qwen25_14b_instruct_lm_target_mini_phi_4_instruct_judge_detoxify'\
+ 'attack_toxic_attack_qwen25_14b_instruct_lm_target_llama_31_8B_instruct_judge_detoxify'
+ 'attack_toxic_attack_qwen25_14b_instruct_lm_target_gemma3_4b_it_judge_detoxify'
  )
 #seed_ranges=("0,50")
 seed_ranges=("0,10" "10,20" "20,30" "30,40" "40,50")
 #budget_per_sample=(5 6 7 8 9 10 15 20 25 30 35 40 45 50 100 200)
 #budget_per_sample=(5 6 7 8 9 10 30 50 100 200)
 budget_per_sample=(10 20)
-budget_per_sample=(20)
+#budget_per_sample=(20)
 for setup in "${setups[@]}"; do
   for seed_range in "${seed_ranges[@]}"; do
     for budget in "${budget_per_sample[@]}"; do
@@ -31,9 +31,9 @@ for setup in "${setups[@]}"; do
 done
 
 setups=( 'attack_toxic_attack_qwen25_14b_instruct_lm_target_qwen25_14b_instruct_judge_detoxify'
-# 'attack_toxic_attack_qwen25_14b_instruct_lm_target_mini_phi_4_instruct_judge_detoxify'\
-# 'attack_toxic_attack_qwen25_14b_instruct_lm_target_llama_31_8B_instruct_judge_detoxify'\
-#  'attack_toxic_attack_qwen25_14b_instruct_lm_target_gemma3_4b_it_judge_detoxify'
+ 'attack_toxic_attack_qwen25_14b_instruct_lm_target_mini_phi_4_instruct_judge_detoxify'\
+ 'attack_toxic_attack_qwen25_14b_instruct_lm_target_llama_31_8B_instruct_judge_detoxify'\
+  'attack_toxic_attack_qwen25_14b_instruct_lm_target_gemma3_4b_it_judge_detoxify'
  )
 budget_per_sample=(10 20)
 for setup in "${setups[@]}"; do
